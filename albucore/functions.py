f0a165d803f6bbe7d75cad6ddd1ca1920402a23a
Vladimir Iglovikov <iglovikov@gmail.com>
2024-05-12 17:22:12 -0700

    Added lut to uint8 images

--- albucore/functions.py
+++ albucore/functions.py
@@ -1,9 +1,19 @@
-__all__ = ["multiply_by_constant"]
 from typing import cast
 
-from albucore.utils import FloatOrUIntArray, clipped
+import cv2
+import numpy as np
+
+from albucore.utils import MAX_VALUES_BY_DTYPE, FloatOrUIntArray, clipped
+
+__all__ = ["multiply_by_constant"]
 
 
 @clipped
 def multiply_by_constant(img: FloatOrUIntArray, value: float) -> FloatOrUIntArray:
+    if img.dtype == np.uint8:
+        max_value = MAX_VALUES_BY_DTYPE[img.dtype]
+        lut = np.arange(0, max_value + 1) * value
+        lut = np.clip(lut, 0, max_value).astype(img.dtype)
+        return cv2.LUT(img, lut)
+
     return cast(FloatOrUIntArray, img * value)
