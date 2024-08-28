d3248f083a0fd6d28b51b8aa71010369ec464883
Daniel Bershatsky <daniel.bershatsky@gmail.com>
2024-08-28 23:26:13 +0300

    Drop upper version bound on `numpy` package
    There are multiple issues to relax version restrictions on `numpy`
    package. Firstly, [NEP-29][1] defines deprecation policy and support
    timelines. It essentially says that `numpy~=1.24` will be deprecated on
    Dec 18, 2024 while `numpy~=1.23` has already been deprecated on Jun 22,
    2024. Secondly, there is no actual need in upper version bound since
    `albucore` package does not interact directly with NumPy C API and does
    not use any deprecated NumPy Python API. The latter can be easily
    checked with `ruff` as follows.
    
    ```shell
    ruff check --select NPY albucore benchmark
    ```
    
    However, it produces deprecation warnings [NPY002][2] for tests due to
    use of `numpy.random.randn` routine. NPY002 warning is not critical at
    the moment but should be eventually resolved.
    
    ```shell
    ruff check --select NPY tests
    ```
    
    [1]: https://numpy.org/neps/nep-0029-deprecation_policy.html#drop-schedule
    [2]: https://docs.astral.sh/ruff/rules/numpy-legacy-random/

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from pkg_resources import DistributionNotFound, get_distribution
 from setuptools import find_packages, setup
 
 INSTALL_REQUIRES = [
-    "numpy>=1.24.4,<2",
+    "numpy>=1.24",
     "typing-extensions>=4.9.0; python_version<'3.10'"
 ]
 
