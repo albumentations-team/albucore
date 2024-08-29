1582a338e1827c8bfbe920dc92e60debba653d62
Vladimir Iglovikov <ternaus@users.noreply.github.com>
2024-08-28 19:14:57 -0700

    Version boosted to 0.0.14 (#27)
    * Version boosted to 0.0.14
    
    * removd benchmark and tests from package
    
    * Cleanup
    
    * updated benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Check if the data directory is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <data_directory>"
+    exit 1
+fi
+
+# Store the data directory path from the command-line argument
+data_dir="$1"
+
 # Define the array of channel values
 channels=(1 3 5)
 
@@ -10,7 +19,7 @@ types=("uint8" "float32")
 for ch in "${channels[@]}"; do
     # Nested loop over each image type
     for type in "${types[@]}"; do
-        # Command to run your program, e.g., a Python script
-        python -m benchmark.benchmark --num_channels $ch --img_type $type --markdown -n 2000 --show-std -r 5 -d /home/vladimir/workspace/evo970/data/Imagenet1k/ILSVRC2015/Data/CLS-LOC/val
+        # Command to run your program, using the provided data directory
+        python -m benchmark.benchmark --num_channels $ch --img_type $type --markdown -n 2000 --show-std -r 5 -d "$data_dir"
     done
 done
