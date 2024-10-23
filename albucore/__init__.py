3e462d5ddea99c12bd5e04462fa1f3e062bc5c4c
Vladimir Iglovikov <iglovikov@gmail.com>
2024-10-23 11:10:38 -0700

    Updated CI

--- albucore/__init__.py
+++ albucore/__init__.py
@@ -1,7 +1,14 @@
-__author__ = "Vladimir Iglovikov"
-__maintainer__ = "Vladimir Iglovikov"
+from importlib.metadata import metadata
 
-__version__ = "0.0.19"
+try:
+    _metadata = metadata("albucore")
+    __version__ = _metadata["Version"]
+    __author__ = _metadata["Author"]
+    __maintainer__ = _metadata["Maintainer"]
+except Exception:  # noqa: BLE001
+    __version__ = "unknown"
+    __author__ = "Vladimir Iglovikov"
+    __maintainer__ = "Vladimir Iglovikov"
 
 from .decorators import *
 from .functions import *
